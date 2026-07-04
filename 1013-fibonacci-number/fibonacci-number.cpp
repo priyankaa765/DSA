25927bd4048d7b0fad5fd61aade24f3c2dfe6a87
priyankaa765 <pk5167206@gmail.com>
2026-07-04 17:47:46 +0530

    Time: 2 ms (78.72%) | Memory: 7.9 MB (37.63%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0){
+            return 0;
+        }else if (n == 1){
+            return 1;
+        }
+
+        int firstT = 0;
+        int secT = 1;
+
+        for( int i =1; i <=n ; i++){
+            int thirdT = firstT + secT ;
+
+            firstT = secT;
+            secT = thirdT;
+        }
+        return firstT;
+    }
+};
