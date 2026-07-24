041cb77a07a6c7c192d4f52c82d8f65fba1789d7
priyankaa765 <pk5167206@gmail.com>
2026-07-24 11:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (69.10%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int count = 0 ;
+
+        for(int i = s.size()-1 ; i >= 0 ; i--){
+
+            char ch = s[i]; // s.at(i)
+
+            if(ch != ' '){
+                count++;
+            }else if(count != 0){
+                break;
+            }
+        }
+
+        return count++;
+        
+    }
+};
