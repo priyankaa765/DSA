4553ff9a888671f8303b76774ea5c83aacf32bd4
Priyanka <pk5167206@gmail.com>
2026-08-16 16:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (63.98%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+     vector<int> ans ;
+     helper(root, ans, 0 ); 
+     return ans;  
+    }
+    
+    // Modified preorder Traversal -->Node -->right -->left
+    public:
+     void helper(TreeNode* root , vector<int>& ans , int level ){
+        if(root == nullptr){
+            return ;
+        }
+        if(ans.size() == level){
+            ans.push_back(root->val);
+        }
+        helper(root->right , ans , level+1);
+        helper(root->left , ans , level+1);
+    }
+};
